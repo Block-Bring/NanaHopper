ab0df1505fc802e6a792ffea02240fb9a670e6f8
Block-Bring <zanht521@qq.com>
2026-08-23 21:20:31 +0800

    Initial commit: Paper plugin project setup

---
+++ src/main/java/top/imbring/nanaHopper/NanaHopper.java
@@ -0,0 +1,17 @@
+package top.imbring.nanaHopper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NanaHopper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
